1d4afa93a85c7d2bd1f0def6616fee19fc8c7528
MD-Catumbela <mundodigitalcz@gmail.com>
2025-03-06 21:13:05 +0100

    Menu abre e fecha

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -5,3 +5,9 @@ btnMenu.addEventListener('click', ()=>{
     menu.classList.toggle('abrir-menu'); // Alterna a classe ao clicar
 });
 
+
+menu.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu'); // Alterna a classe ao clicar
+});
+
+
