026401cc5ad6c33a8d4ac43f4724b1fa4a5720b3
MD-Catumbela <mundodigitalcz@gmail.com>
2025-03-06 21:22:45 +0100

    Feito

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,13 +1,20 @@
 let btnMenu = document.getElementById('btn-menu');
 let menu = document.getElementById('menu-mobile');
+let overlay = document.getElementById('overlay-menu');
 
-btnMenu.addEventListener('click', ()=>{
+btnMenu.addEventListener('click', () => {
     menu.classList.toggle('abrir-menu'); // Alterna a classe ao clicar
 });
 
 
-menu.addEventListener('click', ()=>{
+menu.addEventListener('click', () => {
     menu.classList.remove('abrir-menu'); // Alterna a classe ao clicar
 });
+
+overlay.addEventListener('click', () => {
+    menu.classList.remove('abrir-menu'); // Alterna a classe ao clicar
+});
+
+
 
 
